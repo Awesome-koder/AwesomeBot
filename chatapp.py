88fdbb7b8fbeecafd5bfe8342de56669ffa798c8
Awesome-koder <126327869+Awesome-koder@users.noreply.github.com>
2024-03-28 19:40:42 +0530

    Update chatapp.py

--- chatapp.py
+++ chatapp.py
@@ -36,7 +36,6 @@ def get_vector_store(text_chunks):
     embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
     vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
     vector_store.save_local("faiss_index")
-    print(vector_store)
 
 
 def get_conversational_chain():
