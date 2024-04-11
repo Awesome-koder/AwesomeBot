6fb0ca83311cc2de8bc04d085eefd82fd3d9a866
Awesome-koder <126327869+Awesome-koder@users.noreply.github.com>
2024-04-11 13:10:05 +0530

    Update chatapp.py

--- chatapp.py
+++ chatapp.py
@@ -73,7 +73,7 @@ def get_conversational_chain():
     Answer:
 
     """
-    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3)
+    model = ChatGoogleGenerativeAI(model="gemini-pro-vision", temperature=0.3)
     prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
     chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
     return chain
