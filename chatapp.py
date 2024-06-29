1b9643376eb0be15208badcb12d2972816f0b88a
Awesome-koder <126327869+Awesome-koder@users.noreply.github.com>
2024-06-29 13:46:12 +0530

    Update chatapp.py

--- chatapp.py
+++ chatapp.py
@@ -1,11 +1,10 @@
 import streamlit as st
-import langchain_community
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
-from langchain_community.vectorstores.faiss import FAISS
+from langchain.vectorstores.faiss import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
